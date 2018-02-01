24a051d0a97cb2cf64e67a3e671eb4a23b893d54
Chuck D'Antonio <c_dantonio@acm.org>
2018-02-01 15:41:26 -0500

    Added initial cachability

--- src/main/kotlin/io/pivotal/cambridge/pccworkshop/Application.kt
+++ src/main/kotlin/io/pivotal/cambridge/pccworkshop/Application.kt
@@ -2,9 +2,8 @@ package io.pivotal.cambridge.pccworkshop
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Import
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
-import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -13,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController
 @SpringBootApplication
 @RestController
 @EnableJpaRepositories
+@EnableCaching
 class Application {
     @RequestMapping("/greeting")
     fun hello( @RequestParam(name="name", defaultValue="World") name : String ) : String {
--- src/main/kotlin/io/pivotal/cambridge/pccworkshop/repository/CityRepository.kt
+++ src/main/kotlin/io/pivotal/cambridge/pccworkshop/repository/CityRepository.kt
@@ -5,6 +5,27 @@ import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
 import io.pivotal.cambridge.pccworkshop.domain.City
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.query.Param
+import org.springframework.data.rest.core.annotation.RestResource
+import org.springframework.cache.annotation.Cacheable;
 
 @RepositoryRestResource(collectionResourceRel = "cities", path = "cities")
-interface CityRepository : PagingAndSortingRepository<City, Long>
+interface CityRepository : PagingAndSortingRepository<City, Long> {
+    @Cacheable("cities")
+    @RestResource(path = "name", rel = "name")
+    fun findByNameIgnoreCase(@Param("q") name: String, pageable: Pageable): Page<City>
+
+    @Cacheable("cities")
+    @RestResource(path = "nameContains", rel = "nameContains")
+    fun findByNameContainsIgnoreCase(@Param("q") name: String, pageable: Pageable): Page<City>
+
+    @Cacheable("cities")
+    @RestResource(path = "state", rel = "state")
+    fun findByStateCodeIgnoreCase(@Param("q") stateCode: String, pageable: Pageable): Page<City>
+
+    @Cacheable("cities")
+    @RestResource(path = "postalCode", rel = "postalCode")
+    fun findByPostalCode(@Param("q") postalCode: String, pageable: Pageable): Page<City>
+}
