6d8ffcd9f2a888a3e4fc11b748cd5be8916613b5
Chuck D'Antonio <c_dantonio@acm.org>
2018-02-04 20:31:21 -0500

    Make caching visible by slowing down the repository

--- src/main/kotlin/io/pivotal/cambridge/pccworkshop/repository/CityRepository.kt
+++ src/main/kotlin/io/pivotal/cambridge/pccworkshop/repository/CityRepository.kt
@@ -1,18 +1,20 @@
 package io.pivotal.cambridge.pccworkshop.repository
 
 
-import org.springframework.data.repository.PagingAndSortingRepository
-import org.springframework.data.rest.core.annotation.RepositoryRestResource
-
 import io.pivotal.cambridge.pccworkshop.domain.City
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RestResource
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component
 
-@RepositoryRestResource(collectionResourceRel = "cities", path = "cities")
+@Component
 interface CityRepository : PagingAndSortingRepository<City, Long> {
+    @Cacheable("cities")
+    override fun findAll(): MutableIterable<City>
+
     @Cacheable("cities")
     @RestResource(path = "name", rel = "name")
     fun findByNameIgnoreCase(@Param("q") name: String, pageable: Pageable): Page<City>
--- src/main/kotlin/io/pivotal/cambridge/pccworkshop/repository/SlowCityRepository.kt
+++ src/main/kotlin/io/pivotal/cambridge/pccworkshop/repository/SlowCityRepository.kt
@@ -1,4 +1,26 @@
 package io.pivotal.cambridge.pccworkshop.repository
 
-class SlowCityRepository ( delegate : CityRepository ) : CityRepository by delegate {
+import io.pivotal.cambridge.pccworkshop.domain.City
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
+
+@RepositoryRestResource(collectionResourceRel = "cities", path = "cities")
+class SlowCityRepository ( @Autowired val cityRepository : CityRepository ) : CityRepository by cityRepository {
+
+    override fun findAll(): MutableIterable<City> {
+        simulateSlowService()
+        println("In the slow repository")
+        return cityRepository.findAll()
+    }
+
+    // Don't do this at home
+    private fun simulateSlowService() {
+        try {
+            val time = 3000L
+            Thread.sleep(time)
+        } catch (e: InterruptedException) {
+            throw IllegalStateException(e)
+        }
+
+    }
 }
